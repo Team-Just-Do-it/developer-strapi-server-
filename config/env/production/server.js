bd05e92c6a4455f86e0032122a6a75ef9441e467
Luceta <57941164+Luceta@users.noreply.github.com>
2022-03-26 01:59:30 +0900

    Update: yarn lock and keys

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,3 +1,6 @@
 module.exports = ({ env }) => ({
-  url: env("https://downtoearth3.herokuapp.com/");
+  url: env("https://downtoearth3.herokuapp.com"),
+  app: {
+    keys: env.array("APP_KEYS", ["testKey1", "testKey2"]),
+  },
 });
